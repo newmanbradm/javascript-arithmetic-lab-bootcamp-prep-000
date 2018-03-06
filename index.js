fbc613cd020d7b3d4956ce922d132f6c5822c2c0
Brad Newman <newmanbradm@gmail.com>
2018-03-06 02:41:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function multiply (a, b) {
 function divide(a, b) {
   return a / b
 }
-function inc(a) {
-  return a += 1
+function inc(n) {
+  return n += 1
 }
-function dec(a) {
-  return a -= 1
+function dec(n) {
+  return n -= 1
 }
